213889f7eedcbaba8568e8a6d8af9f126f9506a9
3veryday <hyunseo328@ewhain.net>
2025-04-07 19:26:29 +0900

    web config

--- MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/WebConfig.java
+++ MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig implements WebMvcConfigurer{
                 .setViewName("forward:/index.html");
         registry.addViewController("/**/{spring:\\w+}")
                 .setViewName("forward:/index.html");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.json|\\.png|\\.jpg)$}")
                 .setViewName("forward:/index.html");
     }
 }
