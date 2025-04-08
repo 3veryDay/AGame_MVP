c988f48eb5ba7ee34c2345766569444788613fd1
3veryday <hyunseo328@ewhain.net>
2025-04-08 09:26:07 +0900

    rebuild

--- MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/ViewController.java
+++ MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/ViewController.java
@@ -1,13 +1,23 @@
 package com.example.mvp_agame_spring;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-    @RequestMapping(value = "/{path:^(?!api).*$}")
-    public String forward() {
+
+    @RequestMapping(value = {
+            "/", "/menu", "/dashboard", "/interval", "/interval-entry", "/interval3", "/login"
+    })
+    public String forward(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+
+        // /api로 시작하면 무시 (Spring이 처리)
+        if (uri.startsWith("/api") || uri.startsWith("/static") || uri.startsWith("/index.html")) {
+            return null;
+        }
+
         return "forward:/index.html";
     }
-
 }
