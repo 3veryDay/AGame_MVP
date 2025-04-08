ad97dec7cdd8d05bfb718d91f3ee5acc6cfc3eba
3veryday <hyunseo328@ewhain.net>
2025-04-08 09:12:17 +0900

    rebuild

--- MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/ViewController.java
+++ MVP_Final/MVP_AGame_SPRING/src/main/java/com/example/mvp_agame_spring/ViewController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-    @RequestMapping(value = "/{path:^(?!api).*$}")
+    @RequestMapping({
+            "/", "/menu", "/dashboard", "/interval", "/interval-entry", "/interval3", "/login"
+    })
     public String forward() {
         return "forward:/index.html";
     }
+
 }
